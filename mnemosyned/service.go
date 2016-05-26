0efe61567f0ada8d56ff58de161352725c047a1f
Piotr Kowalczuk <p.kowalczuk.priv@gmail.com>
2016-05-26 12:15:32 +0200

    sql driver registration moved away from daemon package

--- mnemosyned/service.go
+++ mnemosyned/service.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/metrics/prometheus"
-	_ "github.com/lib/pq"
 	"github.com/piotrkowalczuk/sklog"
 	stdprometheus "github.com/prometheus/client_golang/prometheus"
 )

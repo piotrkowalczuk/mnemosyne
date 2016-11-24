8982a34a915c8744ef7331528be5e9e7536bacc0
Piotr Kowalczuk <p.kowalczuk.priv@gmail.com>
2016-11-24 20:31:40 +0100

    docker-entrypoint.sh expose MNEMOSYNED_TTL and MNEMOSYNED_TTC variables

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -3,6 +3,8 @@ set -e
 
 : ${MNEMOSYNED_PORT:=8080}
 : ${MNEMOSYNED_HOST:=0.0.0.0}
+: ${MNEMOSYNED_TTL:=24m}
+: ${MNEMOSYNED_TTC:=1m}
 : ${MNEMOSYNED_CLUSTER_LISTEN:=$(hostname):$MNEMOSYNED_PORT}
 : ${MNEMOSYNED_CLUSTER_SEEDS:=}
 : ${MNEMOSYNED_LOG_ADAPTER:=stdout}
@@ -19,6 +21,8 @@ set -e
 if [ "$1" = 'mnemosyned' ]; then
 exec mnemosyned -host=$MNEMOSYNED_HOST \
 	-port=$MNEMOSYNED_PORT \
+	-ttl=$MNEMOSYNED_TTL \
+	-ttc=$MNEMOSYNED_TTC \
 	-cluster.listen=$MNEMOSYNED_CLUSTER_LISTEN \
 	-cluster.seeds=$MNEMOSYNED_CLUSTER_SEEDS \
 	-storage=$MNEMOSYNED_STORAGE \

7d0dfc738abb03f4247c373e5ede198559f5be68
Piotr Kowalczuk <p.kowalczuk.priv@gmail.com>
2017-04-15 03:03:19 +0200

    python client library and pypi integration

--- mnemosynerpc/doc.go
+++ mnemosynerpc/doc.go
@@ -1,7 +1,8 @@
 // Package mnemosynerpc ...
 package mnemosynerpc
 
-//go:generate protoc -I=. -I=../vendor --go_out=plugins=grpc:. session.proto
+//go:generate protoc -I=. -I=/usr/include -I=../vendor --go_out=plugins=grpc:. session.proto
+//go:generate python -m grpc_tools.protoc -I=. -I=/usr/include -I=../vendor --python_out=. --grpc_python_out=. session.proto
 //go:generate goimports -w session.pb.go
 //go:generate mockery -output=../mnemosynetest -output_file=mnemosynerpc.go -output_pkg_name=mnemosynetest -name=SessionManagerClient
 //go:generate goimports -w ../mnemosynetest/mnemosynerpc.go
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+from subprocess import check_output
+
+version = check_output(["git","describe","--tags"])
+
+setup(
+	name='mnemosyne-client',
+	version=version[1:],
+	description='mnemosyne service grpc client library',
+	url='http://github.com/piotrkowalczuk/mnemosyne',
+	author='Piotr Kowalczuk',
+	author_email='p.kowalczuk.priv@gmail.com',
+	license='MIT',
+	packages=['mnemosynerpc'],
+	install_requires=[
+		'protobuf',
+		'grpcio',
+	],
+  	zip_safe=False,
+  	keywords=['mnemosyne', 'grpc', 'session', 'service', 'client'],
+  	download_url='https://github.com/piotrkowalczuk/mnemosyne/archive/%s.tar.gz' % version.rstrip(),
+  )

0a5525295b28db5117000b970870ada9d5cd7abd
Piotr Kowalczuk <p.kowalczuk.priv@gmail.com>
2018-08-22 17:16:32 +0200

    internal/model/backpack.go - comments

--- internal/model/backpack.go
+++ internal/model/backpack.go
@@ -7,6 +7,8 @@ import (
 	"errors"
 )
 
+// Bag is a simple abstraction on the top of a map.
+// It can be stored in a SQL database as a gob BLOB.
 type Bag map[string]string
 
 // Scan satisfy sql.Scanner interface.
@@ -32,14 +34,17 @@ func (b Bag) Value() (driver.Value, error) {
 	return buf.Bytes(), nil
 }
 
+// Set assigns the value to a given key.
 func (b *Bag) Set(key, value string) {
 	(*b)[key] = value
 }
 
+// Get returns a value for the given key.
 func (b *Bag) Get(key string) string {
 	return (*b)[key]
 }
 
+// Has checks if the given key is set.
 func (b *Bag) Has(key string) bool {
 	_, ok := (*b)[key]
 
